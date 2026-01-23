85f014ca42c36978ee030911a5c1bbcec265ef32
Kirill Taratun <taratunse@gmail.com>
2026-01-23 20:18:25 +0700

    добавление компонентов UserHero, UserBio и страницы профиля пользователя

---
+++ components/users/UserHero.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import useUser from "@/hooks/useUser";
+import Image from "next/image";
+import Avatar from "@/components/Avatar";
+
+interface UserHeroProps {
+    userId: string;
+}
+
+const UserHero = ({ userId }: UserHeroProps) => {
+    const { data: fetchedUser } = useUser(userId)
+    return (
+        <div>
+            <div className='bg-neutral-700 h-44 relative'>
+                { fetchedUser?.coverImage && (
+                    <Image src={fetchedUser.coverImage} fill alt='Cover Image' style={{ objectFit: 'cover' }} />
+                )}
+                <div className='absolute -bottom-16 left-4'>
+                    <Avatar userId={userId} hasBorder isLarge />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserHero;
