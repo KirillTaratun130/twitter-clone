b3dca634e3838506107a326e153f2316ad93a5ba
Kirill Taratun <taratunse@gmail.com>
2026-01-27 18:50:23 +0700

    интеграция функционала подписок в компонент UserBio
    - добавление хука useFollow для управления подписками на пользователей
    - динамическое изменение текста кнопки (Follow/Unfollow) в зависимости от статуса
    - переключение стиля кнопки между secondary и outline для разных состояний
    - вызов toggleFollow при клике на кнопку подписки

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -6,6 +6,7 @@ import { format } from "date-fns";
 import Button from "@/components/Button";
 import {BiCalendar} from "react-icons/bi";
 import useEditModal from "@/hooks/useEditModal";
+import useFollow from "@/hooks/useFollow";
 
 interface UserBioProps {
     userId: string;
@@ -16,6 +17,8 @@ const UserBio = ({ userId }: UserBioProps) => {
     const { data: fetchedUser } = useUser(userId);
 
     const editModal = useEditModal();
+
+    const { isFollowing, toggleFollow } = useFollow(userId);
 
     const createdAt = useMemo(() => {
         if (!fetchedUser?.createdAt) {
@@ -31,7 +34,11 @@ const UserBio = ({ userId }: UserBioProps) => {
                 { currentUser?.id == userId ? (
                     <Button secondary label='Edit' onClick={editModal.onOpen}  />
                 ) : (
-                    <Button label='Follow' onClick={() => {}} secondary />
+                    <Button
+                        label={isFollowing ? 'Unfollow' : 'Follow'}
+                        onClick={toggleFollow}
+                        secondary={!isFollowing}
+                        outline={isFollowing} />
                 )}
             </div>
             <div className='mt-8 px-4'>
